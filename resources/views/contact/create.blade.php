6864728204ee3b678f46096acc0a063aeb6b6004
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-11 16:44:02 +0900

    fix page composition.

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -5,8 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -24,7 +22,6 @@
                         </div>
                     @endif
 
-                    createです。
                     <form method="post" action="{{route('contact.store')}}">
                         @csrf
                         氏名
